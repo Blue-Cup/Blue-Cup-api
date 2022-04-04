12de335bfc7dc27e61045c347e8e71cb5ba42c6e
Matthew Hirth <hirth.24@osu.edu>
2022-04-04 12:08:52 -0400

    Configured the API

--- Blue.Cup.Api/Program.cs
+++ Blue.Cup.Api/Program.cs
@@ -1,8 +1,14 @@
+using Blue.Cup.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(options => options.UseSqlite("Data Source=../Registrar.sqlite", b => b.MigrationsAssembly("Blue.Cup.Api")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
