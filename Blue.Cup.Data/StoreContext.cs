40959602f7d9820fccacf7bc41b8be38b1cba0a7
Matthew Hirth <hirth.24@osu.edu>
2022-04-06 11:20:37 -0400

    Generated OrdersController

--- Blue.Cup.Data/StoreContext.cs
+++ Blue.Cup.Data/StoreContext.cs
@@ -1,4 +1,5 @@
 ﻿using Blue.Cup.Domain.Catalog;
+using Blue.Cup.Domain.Orders;
 using Microsoft.EntityFrameworkCore;
 
 namespace Blue.Cup.Data
@@ -8,6 +9,7 @@ namespace Blue.Cup.Data
         public StoreContext(DbContextOptions<StoreContext> options) : base(options){}
 
         public DbSet<Item>? Items { get; set; }
+        public DbSet<Order>? Orders { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
