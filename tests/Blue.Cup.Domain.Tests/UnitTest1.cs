2d2dccdb293c77957ef7b4d0508584e52fae66b1
Matthew Hirth <hirth.24@osu.edu>
2022-04-11 18:38:39 -0400

    Solution Updated

---
+++ tests/Blue.Cup.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Blue.Cup.Domain.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
